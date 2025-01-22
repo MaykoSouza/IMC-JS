569e0b9b8693cb6edc595fe6916bb7d4ff38efe2
Mayko Souza <71098099+MaykoSouza@users.noreply.github.com>
2025-01-22 18:11:32 -0300

    Aplicação da lógica do IMC

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-const form = document.querySelector('#formulario');
+const form = document.getElementById('formulario')
+const peso = document.getElementById('peso')
+const  altura = document.getElementById('altura')
+const resultado = document.getElementById('resultadoCalculo')
+const calcular = document.getElementById('calcular')
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
+
+    calcular.addEventListener("click", ()=>{
+
+        const num1 = parseFloat(peso.value)
+        const num2 = parseFloat(altura.value)
+        const divisao = num1 / num2
+        
+        resultado.textContent = divisao
+    })
+})
