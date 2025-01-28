1fce6342d1d88b472b9a827c4de46408afb7aa00
Mayko Souza <maykosouzadev@gmail.com>
2025-01-28 15:00:47 -0300

    add table

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const peso = document.getElementById('peso')
 const  altura = document.getElementById('altura')
 const resultado = document.getElementById('resultadoCalculo')
 const calcular = document.getElementById('calcular')
+const magreza = document.getElementById('magreza')
 
 form.addEventListener('submit', (event) =>{
     event.preventDefault();
@@ -13,6 +14,19 @@ form.addEventListener('submit', (event) =>{
         const num2 = parseFloat(altura.value)
         const divisao =  num1 / (num2 * num2 )
         
-        resultado.textContent = divisao
+        resultado.textContent = divisao.toFixed(2)
+
+        
+        if(divisao < 18.5){
+
+            magreza.classList.add('azul')
+
+        }
+
     })
+  
+
+
 })
+
+
